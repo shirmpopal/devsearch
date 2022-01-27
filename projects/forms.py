2dab23931fc10d01e2a51ff0e72d6c34f1e425e0
Shir Muhammad <shirmpopal@gmail.com>
2022-01-27 12:26:37 +0500

    Apply theme to project form.

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,7 @@
+from tkinter import Widget
 from django.forms import ModelForm 
 from .models import Project 
+from django import forms 
 
 class ProjectForm(ModelForm):
   class Meta:
@@ -13,3 +15,11 @@ class ProjectForm(ModelForm):
       'source_link',
       'tags',
     ]
+    widgets = {
+      'tags': forms.CheckboxSelectMultiple(),
+    }
+
+  def __init__(self, *args, **kwargs):
+    super(ProjectForm, self).__init__(*args, **kwargs)
+    for field in self.fields.values():
+      field.widget.attrs.update({'class': 'input'})
