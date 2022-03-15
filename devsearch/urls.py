5c4ccdc06550a1894d266d14f6cfbf7f7ada919e
Shir Muhammad <shirmpopal@gmail.com>
2022-03-15 12:09:24 +0500

    Add reset password functionality for users.

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('projects/', include('projects.urls')),
+
+    path('password_reset', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
+    path('password_reset_sent', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='set_new_password.html'), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
